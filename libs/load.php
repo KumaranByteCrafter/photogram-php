fbc582ab22b4032000beb2073da2eda727b1c004
KumaranByteCrafter <kumaranravi31120032@gmail.com>
2024-02-11 13:10:54 +0530

    chunck the index.php file into mutliple

---
+++ libs/load.php
@@ -0,0 +1,5 @@
+<?php
+function load_template($name){
+    include $_SERVER['DOCUMENT_ROOT']."/practice/app/_templates/$name.php"; //not consistent
+}
+?>
---
+++ test.php
@@ -0,0 +1,14 @@
+<pre>
+<?php
+print_r($_SERVER);
+print("get");
+print_r($_GET);
+print("post");
+print_r($_POST);
+print("files");
+print_r($_FILES);
+print("cookie");
+print_r($_COOKIE);
+
+?>
+</pre>

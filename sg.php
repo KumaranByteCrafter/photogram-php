49800fe71a93de1c86e5cea66f84f1fd59532af0
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-02-19 09:22:26 +0000

    practice about session and cookies

--- sg.php
+++ sg.php
@@ -2,9 +2,18 @@
 <?php
 session_start();
 include "libs/load.php";
-setcookie("testcookie","testvalue",time() + (8600 *30),"/");
+//setcookie("testcookie","testvalue",time() + (8600 *30),"/");
+print_r($_SERVER);
 print("session\n");
 print_r($_SESSION);
+if(isset($_GET['clear'])){
+    printf("clearing..\n");
+    session_unset();
+}
+if(isset($_GET['destroy'])){
+    printf("clearing..\n");
+    session_destroy();
+}
 if(isset($_SESSION['a'])){
     print("existing values is $_SESSION[a] \n");
 }else{

09bf8f940659c5fe95c46f7b2204825666a174fc
KumaranByteCrafter <kumaranravi3112003@gmail.com>
2024-07-15 06:16:33 +0000

    sql commands

--- __migration.sql
+++ __migration.sql
@@ -1,3 +1,31 @@
---14 Jul 2024 
-ALTER TABLE `auth`
-ADD `sec_email` varchar(256) NULL;
+DROP TABLE IF EXISTS `auth`;
+CREATE TABLE `auth` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` tinytext NOT NULL,
+  `email` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `blocked` int NOT NULL DEFAULT '0',
+  `active` tinyint NOT NULL DEFAULT '0',
+  `sec_email` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`(32))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `bio` longtext NOT NULL,
+  `avatar` varchar(1024) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `dob` date DEFAULT NULL,
+  `instagram` varchar(1024) DEFAULT NULL,
+  `twitter` varchar(1024) DEFAULT NULL,
+  `facebook` varchar(1024) DEFAULT NULL,
+  KEY `id` (`id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `auth` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
